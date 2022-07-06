46436a4fd63ae361401f97b58545e6582b45c2c6
Otavio Santos <108561241+Allahur@users.noreply.github.com>
2022-07-06 18:53:54 -0300

    Calendar.py
    Basic

---
+++ Calendar.py
@@ -0,0 +1,17 @@
+conversor_de_meses = {
+
+"1" : "Janeiro",
+"2" : "Fevereiro",
+"3" : "Março",
+"4" : "Abril",
+"5" : "Maio",
+"6" : "Junho",
+"7" : "Julho",
+"8" : "Agosto",
+"9" : "Setembro",
+"10" : "Outubro",
+"11" : "Novembro",
+"12" : "Decembro"
+
+}
+print(conversor_de_meses["11"])
