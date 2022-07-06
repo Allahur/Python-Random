4bf07a934775cd5c6d8311fad7063c9f40f0b4c0
Otavio Santos <108561241+Allahur@users.noreply.github.com>
2022-07-06 19:00:43 -0300

    Criptografia.py
    Um código básico em python de como usar a criptografia.

---
+++ Criptografia.py
@@ -0,0 +1,11 @@
+def cripto(frase):
+    tradutor = ""
+    for letra in frase:
+        if letra in "PeopleName": tradutor = tradutor + "@3v489nt8771@#%@v324tvb"
+        elif letra in "PeopleName": tradutor = tradutor + "$@G$$#%htyyw5rwg5y"
+        elif letra in "PeopleName": tradutor = tradutor + "G@$%$"
+        elif letra in "PeopleName": tradutor = tradutor + "2%$G$%@g"
+        elif letra in "PeopleName": tradutor = tradutor + "%&$$%&¨%&"     
+        else: tradutor = tradutor + letra
+    return tradutor
+print(cripto(input("Digite sua frase: "))) 
